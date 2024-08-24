b34cf4b6d3645120e95a8a2b2a9a078a572b3d1f
Christoph Bless <github@cbless.de>
2024-08-24 21:45:34 +0200

    added version number to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='scandb',
-    version='1.2.0',
+    version='1.2.1',
     author='Christoph Bless',
     author_email='github@cbless.de',
     url='https://github.com/c-bless/scandb',
